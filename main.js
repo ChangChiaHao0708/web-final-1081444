d89c5aaa6e9df6a4beb530ac215092c7f32c1d45
Nemo <nemo070862@gmail.com>
2023-06-13 12:36:33 +0800

    random

--- main.js
+++ main.js
@@ -4,15 +4,31 @@ let flavors = [
     "味噌拉麵",
     "鹽味拉麵"
 ];
-let previousIndex;
+let concentration = [
+    "很濃",
+    "濃",
+    "清爽",
+    "清淡",
+    "很清淡"
+];
+let thickness = [
+    "細麵",
+    "中粗麵",
+    "粗麵"
+];
+let firmness = [
+    "超硬",
+    "偏硬",
+    "普通",
+    "偏軟",
+    "超軟"
+];
 $(function(){
     $("input").on("click", function(){
-        let randomIndex = Math.floor(Math.random()*flavors.length);
-        while(randomIndex == previousIndex)
-        {
-            randomIndex = Math.floor(Math.random()*flavors.length);
-        }
-        previousIndex = randomIndex;
-        $("#flavor").text(flavors[randomIndex]);
+        let randomFlavor = Math.floor(Math.random()*flavors.length);
+        let randomConcentrtion = Math.floor(Math.random()*concentration.length);
+        let randomThickness = Math.floor(Math.random()*thickness.length);
+        let randomFirmness = Math.floor(Math.random()*firmness.length);
+        $("#flavor").text(flavors[randomFlavor]+" "+concentration[randomConcentrtion]+" "+thickness[randomThickness]+" "+firmness[randomFirmness]);
     });
 });
